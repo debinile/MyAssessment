f5ba10c6b63e7f50b1fb2e85d8da9cd48eb2550c
debinile <85246573+debinile@users.noreply.github.com>
2021-06-26 23:35:19 +0100

    Initial commit 2

---
+++ app/src/main/java/com/Ajoritsedebi/myassessment/SignupPage.kt
@@ -0,0 +1,11 @@
+package com.Ajoritsedebi.myassessment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignupPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup_page)
+    }
+}
---
+++ app/src/main/java/com/Ajoritsedebi/myassessment/ThirdPage.kt
@@ -0,0 +1,11 @@
+package com.Ajoritsedebi.myassessment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ThirdPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_third_page)
+    }
+}
